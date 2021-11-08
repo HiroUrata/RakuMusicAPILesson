6cfac880029a6665d0da99026082b63000bd297b
HiroUrata <1712026@i.softbank.jp>
2021-11-08 22:35:04 +0900

    検索からcellの表示まで問題なし

--- RakuMusicAPILesson/Controller/ViewController.swift
+++ RakuMusicAPILesson/Controller/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
 
@@ -13,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var resultCollectionView: UICollectionView!
+    
+    private let collectionViewLayout = UICollectionViewFlowLayout()
+    
+    private let alamofireProcess = AlamofireProcess()
     
     private var cellContentsArray = [MusicDetailModel]()
     
@@ -23,8 +28,35 @@ class ViewController: UIViewController {
         resultCollectionView.delegate = self
         resultCollectionView.dataSource = self
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        collectionViewLayout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        resultCollectionView.collectionViewLayout = collectionViewLayout
+    }
 
-
+    @IBAction func artistNameSearch(_ sender: UIButton) {
+        
+        if searchTextField.text?.isEmpty != true{
+            
+            alamofireProcess.getMusicData(searchKeyword: searchTextField.text) { result, error in
+                
+                if error != nil{
+                    
+                    print(error.debugDescription)
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {[self] in
+                
+                    cellContentsArray = result!
+                    resultCollectionView.reloadData()
+                }
+            }
+        }
+    }
+    
 }
 
 extension ViewController:UICollectionViewDelegate{
@@ -40,13 +72,36 @@ extension ViewController:UICollectionViewDataSource{
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MusicDetailCell", for: indexPath) as! CollectionCustomCell
+        
+        cell.mediumImageView.sd_setImage(with: URL(string: cellContentsArray[indexPath.row].mediumImageUrl!))
+        cell.detailTextView.text = """
+                                   タイトル:
+                                      \(cellContentsArray[indexPath.row].title ?? "読み込みエラー")
+                                   
+                                   アーティスト:
+                                      \(cellContentsArray[indexPath.row].artistName ?? "読み込みエラー")
+                                   
+                                   レーベル:
+                                      \(cellContentsArray[indexPath.row].label ?? "読み込みエラー")
+                                   
+                                   説明文:
+                                      \(cellContentsArray[indexPath.row].itemCaption ?? "読み込みエラー")
+                                   
+                                   """
+        
+        return cell
+    
     }
     
 }
 
 extension ViewController:UICollectionViewDelegateFlowLayout{
     
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: collectionView.frame.width / 2 - 10, height: collectionView.frame.height / 2 - 10)
+    }
 }
 
